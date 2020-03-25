ab36f00883af3bc622f93dea9239d50bf0005a9f
Pierre Glaser <pierreglaser@msn.com>
2020-03-25 14:20:30 +0100

    add a helper function, update DATA_DIR

--- templatematching/templatematching/utils.py
+++ templatematching/templatematching/utils.py
@@ -3,9 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-DATA_DIR = Path(__file__).parent.parent / "images"
-PROCESS_DATA_DIR = Path(__file__).parent.parent / "img_normalized"
-PATCH_DIR = Path(__file__).parent.parent / "patches"
+DATA_DIR = Path(__file__).parent.parent.parent / "images"
+PROCESS_DATA_DIR = Path(__file__).parent.parent.parent / "img_normalized"
+PATCH_DIR = Path(__file__).parent.parent.parent / "patches"
 
 
 def read_pgm(img_no, byteorder=">"):
@@ -80,6 +80,25 @@ def read_patch(img_no, pos_neg='positive', loc='left'):
     patch = np.mean(patch, axis=2)
 
     return patch
+
+
+def load_patches(num_patches, with_labels=True):
+    pos_patches = np.zeros((num_patches, 101, 101))
+    pos_labels = np.ones((num_patches, 1))
+
+    neg_patches = np.zeros((num_patches, 101, 101))
+    neg_labels = np.zeros((num_patches, 1))
+
+    for i in range(num_patches):
+        pos_patch = read_patch(i, loc='left', pos_neg="positive")
+        neg_patch = read_patch(i, loc='left', pos_neg="negative")
+
+        pos_patches[i] = pos_patch
+        neg_patches[i] = neg_patch
+
+    all_patches = np.vstack((pos_patches, neg_patches))
+    all_labels = np.vstack((pos_labels, neg_labels))
+    return all_patches, all_labels
 
 
 def read_norm_img(img_no):
