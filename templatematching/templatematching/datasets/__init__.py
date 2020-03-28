626d91393abaeb56f27e57c54a2509c356415903
Pierre Glaser <pierreglaser@msn.com>
2020-03-28 18:47:02 +0100

    expose main objects in respective submodules

--- templatematching/templatematching/datasets/__init__.py
+++ templatematching/templatematching/datasets/__init__.py
@@ -1,3 +1,4 @@
 from .loading import read_images, read_eye_annotations
+from .data_generation import make_cross, make_circle
 
-__all__ = ['read_images', 'read_eye_annotations']
+__all__ = ["read_images", "read_eye_annotations", "make_cross", "make_circle"]
