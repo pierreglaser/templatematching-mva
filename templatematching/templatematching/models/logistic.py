23a241687d26fcbc2778624cc3941cd077698d09
cchadd <clement.chadebec@orange.fr>
2020-03-29 15:09:05 +0200

    Add SE2 logistic

--- templatematching/templatematching/models/logistic.py
+++ templatematching/templatematching/models/logistic.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.special import expit
-from templatematching.models.linear import R2Ridge
+from templatematching.models.linear import R2Ridge, SE2Ridge
 
 
 # XXX: R2LogReg inheriting from R2Ridge is a hack: the commom
@@ -28,7 +28,7 @@ class R2LogReg(R2Ridge):
             lbd,
             verbose,
             random_state=random_state,
-            eye=eye
+            eye=eye,
         )
         self.model_name = "Logistic Ridge"
         self.max_iter = max_iter
@@ -45,6 +45,91 @@ class R2LogReg(R2Ridge):
 
         # Randomly intialize c
         c = self.rs.rand(Nk * Nl, 1)
+
+        # Normalize c
+        c /= np.linalg.norm(c)
+
+        loss = 1
+
+        # Optimization step
+        for iter_no in range(self.max_iter):
+
+            if loss < self.tol:
+                break
+
+            c_old = c.copy()
+            p = expit(S @ c)
+            w = p * (1 - p)
+
+            W = np.diag(w.flatten())
+
+            # Close form hessian
+            H = -(S.T @ W @ S + self.lbd * R + self.mu * np.eye(S.shape[1]))
+            H_inv = np.linalg.inv(H + self.tol * np.eye(H.shape[0]))
+
+            # Close form gradient
+            grad = S.T @ (y - p) - self.lbd * R @ c - self.mu * np.eye(S.shape[1]) @ c
+            c -= H_inv @ grad
+            c /= np.linalg.norm(c)
+
+            loss = np.linalg.norm(c - c_old)
+
+            if self.verbose:
+                print(f"iteration no: {iter_no}, change in coefs: {loss}")
+        self._S, self._spline_coef = S, c
+
+
+class SE2LogReg(SE2Ridge):
+    def __init__(
+        self,
+        template_shape,
+        splines_per_axis,
+        wavelet_dim,
+        num_orientation_slices=12,
+        spline_order=2,
+        mu=0,
+        lbd=0,
+        Dxi=0,
+        Deta=0,
+        Dtheta=0,
+        max_iter=10,
+        tol=1e-8,
+        random_state=None,
+        eye="left",
+        verbose=0,
+    ):
+        super().__init__(
+            template_shape,
+            splines_per_axis,
+            wavelet_dim,
+            num_orientation_slices,
+            spline_order,
+            mu,
+            lbd,
+            Dxi,
+            Deta,
+            Dtheta,
+            verbose,
+            random_state=random_state,
+            eye=eye,
+        )
+
+        self.model_name = "SE2 Logistic Ridge"
+        self.max_iter = max_iter
+        self.tol = tol
+        self.rs = np.random.RandomState(random_state)
+
+    def _fit_patches(self, X, y):
+        X = self._ost.fit_transform(X).imag  # can also take the modulus
+        self._check_params(X)
+        y = y.reshape(-1, 1)
+
+        _, _, _, _, Nk, Nl, Nm, _, _, _ = self._get_dims()
+        S = self._create_s_matrix(X)
+        R = self._create_r_matrix()
+
+        # Randomly intialize c
+        c = self.rs.rand(Nk * Nl * Nm, 1)
 
         # Normalize c
         c /= np.linalg.norm(c)
