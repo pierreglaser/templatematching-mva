8d02cb96ddab2cb9c7e78fd5e2848d1282f754ba
cchadd <clement.chadebec@orange.fr>
2020-03-30 16:20:04 +0200

    Add models

--- templatematching/templatematching/models/__init__.py
+++ templatematching/templatematching/models/__init__.py
@@ -3,4 +3,4 @@ from .linear import R2Ridge, SE2Ridge
 from .logistic import R2LogReg, SE2LogReg
 
 
-__all__ = ["Averager", "R2Ridge", "SE2Ridge", "R2LogReg", "SE2LogReg"]
+__all__ = ["Averager", "SE2Averager", "R2Ridge", "SE2Ridge", "R2LogReg", "SE2LogReg"]
