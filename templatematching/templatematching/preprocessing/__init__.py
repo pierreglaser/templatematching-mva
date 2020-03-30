d94cb02dd9bb4e298d1dd27dab7114fe6310c78f
cchadd <clement.chadebec@orange.fr>
2020-03-30 14:11:36 +0200

    Black reformating

--- templatematching/templatematching/preprocessing/__init__.py
+++ templatematching/templatematching/preprocessing/__init__.py
@@ -2,4 +2,4 @@ from .image_transformer import Normalizer
 from .patch_creator import PatchCreator
 from .orientation_transformer import OrientationScoreTransformer
 
-__all__ = ['Normalizer', 'PatchCreator', 'OrientationScoreTransformer']
+__all__ = ["Normalizer", "PatchCreator", "OrientationScoreTransformer"]
