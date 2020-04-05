09686189d4c2e31c8beb6b88f3541a39106ba0a3
Pierre Glaser <pierreglaser@msn.com>
2020-04-05 18:15:11 +0200

    parallelize image normalization

--- templatematching/templatematching/preprocessing/image_transformer.py
+++ templatematching/templatematching/preprocessing/image_transformer.py
@@ -1,12 +1,16 @@
 import numpy as np
-from scipy.signal import fftconvolve
 import functools
+
+from scipy.signal import fftconvolve
 from scipy.integrate import dblquad
+from joblib import Parallel, delayed
 
 from .utils import m_function
 
 
-def _normalize_img_batched(image, window, batch_size=50, mask=None, eps=1e-7):
+def _normalize_images_batched(
+    image, window, batch_size=50, mask=None, eps=1e-7, n_jobs=1
+):
     if mask is None:
         mask = np.ones((image.shape[-2], image.shape[-1]))
 
@@ -16,49 +20,44 @@ def _normalize_img_batched(image, window, batch_size=50, mask=None, eps=1e-7):
     mask = mask.astype(int)
     mask_c_window = fftconvolve(mask, window, mode="same")
 
-    img_norm = np.zeros(image.shape)
-
-    for i in range(int(image.shape[0] / batch_size)):
-
-        im_squared = image[i * batch_size : (i + 1) * batch_size, :, :] ** 2
-
-        im_mean = fftconvolve(
-            image[i * batch_size : (i + 1) * batch_size, :, :] * mask,
+    images_normalized = Parallel(backend="threading", n_jobs=n_jobs)(
+        delayed(_normalize_batch)(
+            image[i * batch_size: (i + 1) * batch_size, :, :],  # noqa
             window,
-            mode="same",
-        ) / (mask_c_window + eps)
-        im_mean_sq = fftconvolve(im_squared * mask, window, mode="same") / (
-            mask_c_window + eps
+            mask,
+            eps,
+            mask_c_window,
         )
+        for i in range(int(image.shape[0] / batch_size))
+    )
+    return np.concatenate(images_normalized)
 
-        std = np.sqrt(np.abs(im_mean_sq - im_mean ** 2))
 
-        background = (
-            1
-            - np.abs(image[i * batch_size : (i + 1) * batch_size, :, :] - im_mean)
-            / (std + eps)
-        ) >= 0
-        background = background.astype(int)
-        mask_c_background = fftconvolve(mask * background, window, mode="same")
+def _normalize_batch(images, window, mask, eps, mask_c_window=None):
+    im_squared = images ** 2
 
-        im_mean = fftconvolve(
-            image[i * batch_size : (i + 1) * batch_size, :, :] * mask * background,
-            window,
-            mode="same",
-        ) / (mask_c_background + eps)
-        im_mean_sq = fftconvolve(
-            im_squared * mask * background, window, mode="same"
-        ) / (mask_c_background + eps)
+    im_mean = fftconvolve(images * mask, window, mode="same",) / (
+        mask_c_window + eps
+    )
+    im_mean_sq = fftconvolve(im_squared * mask, window, mode="same") / (
+        mask_c_window + eps
+    )
 
-        std = np.sqrt(np.abs(im_mean_sq - im_mean ** 2))
+    std = np.sqrt(np.abs(im_mean_sq - im_mean ** 2))
 
-        img_norm[i * batch_size : (i + 1) * batch_size, :, :] = np.tanh(
-            8
-            * (image[i * batch_size : (i + 1) * batch_size, :, :] - im_mean)
-            / (std + eps)
-        )
+    background = (1 - np.abs(images - im_mean) / (std + eps)) >= 0
+    background = background.astype(int)
+    mask_c_background = fftconvolve(mask * background, window, mode="same")
 
-    return img_norm
+    im_mean = fftconvolve(images * mask * background, window, mode="same",) / (
+        mask_c_background + eps
+    )
+    im_mean_sq = fftconvolve(
+        im_squared * mask * background, window, mode="same"
+    ) / (mask_c_background + eps)
+
+    std = np.sqrt(np.abs(im_mean_sq - im_mean ** 2))
+    return np.tanh(8 * (images - im_mean) / (std + eps))
 
 
 class Normalizer:
@@ -67,10 +66,11 @@ class Normalizer:
     normalization scheme
     """
 
-    def __init__(self, wind_order=3, wind_radius=10, batch_size=50):
+    def __init__(self, wind_order=3, wind_radius=10, batch_size=50, n_jobs=1):
         self.wind_order = wind_order
         self.wind_radius = wind_radius
         self.batch_size = batch_size
+        self.n_jobs = n_jobs
 
     def fit(self, X, y=None):
 
@@ -90,12 +90,15 @@ class Normalizer:
         )
 
     def transform(self, X):
-        # XXX: we should not cast as int8 as _normalize_img_batched
+        # XXX: we should not cast as int8 as _normalize_images_batched
         # cause int8 overflow. But somehow the preprocessing looks
         # better in this case...
         X = X.astype(np.uint8)
-        transformed_X = _normalize_img_batched(
-            X, self.window, min(X.shape[0], self.batch_size)
+        transformed_X = _normalize_images_batched(
+            X,
+            self.window,
+            min(X.shape[0], self.batch_size),
+            n_jobs=self.n_jobs,
         )
         return transformed_X
 
